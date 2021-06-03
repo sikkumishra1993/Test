8c9ed8251861f8a77458e40dec0e7888683cd361
sikkumishra1993 <49817049+sikkumishra1993@users.noreply.github.com>
2021-06-03 12:39:06 +0530

    for loop added

--- docker.sh
+++ docker.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
-echo "Hello world"
-FILE=test
+#echo "Hello world"
+#FILE=test
 
-if [ -d "$FILE" ]
-then
-  echo "file already exist"
-else
-  mkdir test
-fi
+#if [ -d "$FILE" ]
+#then
+#  echo "file already exist"
+#else
+#  mkdir test
+#fi
 
-read -p "are you 21 or over : " ANSWER
-case "$ANSWER" in
-    [yY] | [yY][eE][sS])
-        echo "you can have beer :)"
-        ;;
-    [nN] | [nN][oO])
-        echo "you can not have beer"
-        ;;
-    *)
-        echo "please enter y/yes or n/no"
-        ;;
-esac
+#read -p "are you 21 or over : " ANSWER
+#case "$ANSWER" in
+#    [yY] | [yY][eE][sS])
+#        echo "you can have beer :)"
+#        ;;
+#    [nN] | [nN][oO])
+#        echo "you can not have beer"
+#        ;;
+#    *)
+#        echo "please enter y/yes or n/no"
+#        ;;
+#esac
+NAMES="Shashi Ravi Rashmi Priyanka"
+
+for NAME in $NAMES
+    do
+        echo "Hello $NAMES"
+done
